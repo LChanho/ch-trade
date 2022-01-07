fc0c3243d00d6a0840eaea80022d43ac84058bb5
LChanho <96819902+LChanho@users.noreply.github.com>
2022-01-07 19:54:07 +0900

    Update alarmRSI.py

--- alarmRSI.py
+++ alarmRSI.py
@@ -3,6 +3,7 @@ import pandas
 import datetime
 import time
 import telepot
+from decimal import *
 
 def rsi(ohlc: pandas.DataFrame, period:int = 14) :
     delta = ohlc['close'].diff()
@@ -17,20 +18,75 @@ def rsi(ohlc: pandas.DataFrame, period:int = 14) :
 
     return pandas.Series(100 - (100/(1 + RS)), name = "RSI")
 
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_profit_price(ticker):
+    price = round(get_current_price(ticker)*1.02)
+    price = price - (price % get_hoga(price))
+    return price
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+def get_hoga(cur_price):
+    try:
+        # 호가 단위
+        if Decimal(str(cur_price)) < 10:
+            hoga_val = 0.01
+        elif Decimal(str(cur_price)) < 100:
+            hoga_val = 0.1
+        elif Decimal(str(cur_price)) < 1000:
+            hoga_val = 1
+        elif Decimal(str(cur_price)) < 10000:
+            hoga_val = 5
+        elif Decimal(str(cur_price)) < 100000:
+            hoga_val = 10
+        elif Decimal(str(cur_price)) < 500000:
+            hoga_val = 50
+        elif Decimal(str(cur_price)) < 1000000:
+            hoga_val = 100
+        elif Decimal(str(cur_price)) < 2000000:
+            hoga_val = 500
+        else:
+            hoga_val = 1000
+        return hoga_val
+    except Exception:
+        raise
+
 tickers = pyupbit.get_tickers(fiat='KRW')
 
 token = '5008461782:AAEqAxUVEIKOYhZAr4gvj1UIqNkN1tCvD7k'
 mc = '1950703241'
-
 bot = telepot.Bot(token)
+
+access = "4eBjPLU8uWxUxE1Axi59axxzHG2AXZZENSJgYJ65"
+secret = "88SvYJoj9em7w7XlV9DCqjNItfXuZtpjJxdfZ91n"
+upbit = pyupbit.Upbit(access, secret)
+
+krw = get_balance("KRW")
 
 while 1:
     for i in range(len(tickers)):
         data = pyupbit.get_ohlcv(ticker=tickers[i], interval="minute5")
         now_rsi = rsi(data, 14).iloc[-1]
         if now_rsi < 30 and tickers[i] != 'KRW-BTT':
-            bot.sendMessage(mc, tickers[i])
+            krw = get_balance("KRW")
+            if krw > 5000:
+                upbit.buy_market_order(tickers[i], 100000)
+                target_price = get_profit_price(tickers[i]) # +2%
+                upbit.sell_limit_order(tickers[i], target_price, get_balance(tickers[i][4:]))
             #print(datetime.datetime.now(), now_rsi)
+            #bot.sendMessage(mc, tickers[i])
             #print(tickers[i])
             #print('RSI : ', now_rsi)
         time.sleep(0.05)
